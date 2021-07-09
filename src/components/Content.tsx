65eebc66f705ef3ba262135d0749b92f720f5c1f
leandrosiq <le_kts12@hotmail.com>
2021-07-09 20:31:26 -0300

    refatorando; adicionando componentização na  aplicação

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,39 @@
-export function Content() {
+
+import { MovieCard } from './MovieCard';
+
+
+interface ContextProps {
+  movies: Array<{
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }>,
+  selectedGenre: {
+    title: string;
+  };
+}
+
+export function Content({ movies, selectedGenre }: ContextProps) {
   // Complete aqui
+
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
